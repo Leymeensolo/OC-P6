eea9809b81aad107dc3f015b0b6574038f2da639
Leymeensolo <95717080+Leymeensolo@users.noreply.github.com>
2022-02-09 21:19:27 +0100

    Update script-p6backup.py

--- script-p6backup.py
+++ script-p6backup.py
@@ -14,10 +14,10 @@ print(str_date_backup)
 # /etc/apache2/
 # /etc/apache2/apache2.conf
 path_input = r'/etc/apache2/' 
-path_output = r’/root/OC-P6/p6backup/’ + '\\' + str_date_backup 
+path_output = r’/root/OC-P6/p6backup/’ + str_date_backup 
 print(path_output) 
 
-copyfile(path_input path_output)
+copytree(path_input path_output)
 
 
 # Copie le contenu du répertoire package php + le contenu du répertoire wordpress vers dossier sauvegarde P6 "/root/OC-P6/p6backup/"
@@ -25,25 +25,25 @@ copyfile(path_input path_output)
 # /var/www/html/wp-config-sample.php
 # /var/www/html/wordpress/
 path_input = r'/var/www/html'
-path_output = r’/root/OC-P6/p6backup/’ + '\\' + str_date_backup 
+path_output = r’/root/OC-P6/p6backup/’ + str_date_backup 
 print(path_output) 
 
-copyfile(path_input path_output)
+copytree(path_input path_output)
 
 
 # Copie le contenu du répertoire fichier de configuration  php vers dossier sauvegarde P6 "/root/OC-P6/p6backup/"
 # /etc/php/7.3
 path_input = r'/etc/php/'
-path_output = r’/root/OC-P6/p6backup/’ + '\\' + str_date_backup 
+path_output = r’/root/OC-P6/p6backup/’ + str_date_backup 
 print(path_output) 
 
-copyfile(path_input path_output)
+copytree(path_input path_output)
 
 
 # Copie le contenu du répertoire base de données (Wordpress / Maria db et MySqL) vers dossier sauvegarde P6 "/root/OC-P6/p6backup/"
 # /tmp/
 path_input = r'/tmp/'
-path_output = r’/root/OC-P6/p6backup/’ + '\\' + str_date_backup 
+path_output = r’/root/OC-P6/p6backup/’ + str_date_backup 
 print(path_output) 
 
-copyfile(path_input path_output)
+copytree(path_input path_output)
